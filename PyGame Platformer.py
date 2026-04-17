dfa4b9d07e9b140b1cee765f9e0f7c79acd96f59
Maliketh1729 <173165@lrgs.org.uk>
2026-04-17 13:45:44 +0100

    Add files via upload
    Better jump physics

--- PyGame Platformer.py
+++ PyGame Platformer.py
@@ -1,8 +1,8 @@
 
-import time, sys, os, PIL, tkinter,pygame
+import time, sys, os, pygame
 from pygame.locals import *                #Allows easier use of pygame functions
-from tkinter import *
-from PIL import Image, ImageTk
+#from tkinter import *
+#from PIL import Image, ImageTk
 pygame.init() #Initializes pyGame
 clock = pygame.time.Clock()         #Limits FPS to 60
 
@@ -19,7 +19,6 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.alive = True
-        self.movey = 0
         self.aniCounter = 0
         self.vertSpeed = 0
         self.curFrame = 0
@@ -29,8 +28,8 @@ class Player(pygame.sprite.Sprite):
         self.aniCycles = 2
 
 
-        self.jumpForce = 1
-        self.gravStrength = 1
+        self.jumpForce = 4   #make negative to be a beaver
+        self.gravStrength = 1 
         self.moveSpeed = 5
         self.accSpeed = 20
         
@@ -46,21 +45,21 @@ class Player(pygame.sprite.Sprite):
             print(self.images)
 
     def gravity(self):
-        if self.rect.y <= worldy - ty - ty:        #If character position above world bottom(in the air):
+        if self.rect.y <= worldy-ty-ty:        #If character position above world bottom(in the air):
             self.accRem -= self.gravStrength         #Acceleration decreases by grav strength
-                                                    #FOUND ISSUE: NOT REGISTERING AS 'ON GROUND'
+                                        #FOUND ISSUE: NOT REGISTERING AS 'ON GROUND'
         else:                                        #else
-            if accRem < 0:                               #if accelleration is negative:
-                self.vertSpeed = 0                           #Stop falling
-                self.accRem = 0
-                
+            if self.accRem < 0:                               #if accelleration is negative:
+                self.vertSpeed = 0                                #Stop falling
+                self.accRem = 0                                   #and kill all momentum
+
                       
             
     def update(self):
         pressed_keys = pygame.key.get_pressed()
         
         if pressed_keys[K_SPACE]:
-            if self.rect.y >= worldy -ty -ty - ty:       #If not in the air(on the ground) and space pressed:
+            if self.rect.y >= worldy -ty -ty:       #If not in the air(on the ground) and space pressed:
                 self.accRem += self.jumpForce               #Accelleration increases by jump force
                 print(self.accRem)                      
 
@@ -76,7 +75,7 @@ class Player(pygame.sprite.Sprite):
         if pressed_keys[K_ESCAPE]:
             running = False
 
-        if self.aniCounter >= 10:                                    #Reduces the speeed of the animation 10x
+        if self.aniCounter >= 20:                                    #Reduces the speeed of the animation 10x
             if self.curFrame >= self.aniCycles:                        #Resets the frame whenever it loops
                 self.curFrame = 0
             self.image = self.images[self.curFrame]
@@ -88,8 +87,8 @@ class Player(pygame.sprite.Sprite):
 
         
         self.aniCounter += 1
-        if self.rect.y > worldy -ty -ty or self.accRem > 0:                           #if player height greater/equal to (lowest screen height - sprite height)
-            self.rect.move_ip(0, -self.vertSpeed)                           #move vertically according to their speed                           
+      #  if self.rect.y > worldy -ty -ty:# or self.accRem > 0:                           #if player height greater/equal to (lowest screen height - sprite height)
+        self.rect.move_ip(0, -self.vertSpeed)                           #move vertically according to their speed                           
     def death(self):
         self.alive = False
 
@@ -115,17 +114,17 @@ class BatVert(pygame.sprite.Sprite):
             self.images.append(img)
             self.image = self.images[self.curFrame]
             self.rect = self.image.get_rect()
-        self.rect.move_ip(worldx // 2, 0)
+        self.rect.move_ip(worldx // 2, worldy //2)
     def update(self):
         if self.rect.y >= worldy - (2 * ty):
             self.upSwing = True
-        if self.rect.y <= worldy // 2 :
+        if self.rect.y <= worldy // 2 + ty :
             self.upSwing = False
             
         if self.upSwing == True:
-            self.rect.move_ip(0, -3)
+            self.rect.move_ip(0, -4)
         if self.upSwing == False:
-            self.rect.move_ip(0, 3)
+            self.rect.move_ip(0, 4)
 
 
         if self.aniCounter >= 10:                                    #Reduces the speeed of the animation 15x
@@ -140,7 +139,7 @@ class BatVert(pygame.sprite.Sprite):
         screen.blit(self.image, self.rect)
 
 class Platform(pygame.sprite.Sprite):
-    def __init__(self,xloc, yloc, imgw, imgh, img):
+    def __init__(self, xloc,yloc, imgw,imgh, img):
         super().__init__()
         self.image = pygame.image.load("pyGame_Image_Folder/Terrain/Platformer/FloatingIsland/Floating_Island_1.png").convert()
         self.rect = self.image.get_rect()
@@ -153,7 +152,7 @@ class Platform(pygame.sprite.Sprite):
         screen.blit(self.image, self.rect)
         
 class Spike(pygame.sprite.Sprite):
-    def __init__(self,xloc, yloc, imgw, imgh, img):
+    def __init__(self, xloc,yloc, imgw,imgh, img):
         super().__init__()
         self.image = pygame.image.load("pyGame_Image_Folder/Animation/Platformer/Obstacles/Spike/Spike1.png").convert()
         self.rect = self.image.get_rect()
@@ -178,18 +177,18 @@ class Level:
                 
     def spike(level, tx, ty):
         spike_list = pygame.sprite.Group()
-        sploc = []                                       #sploc is a list which determines the location of the spikes e.g. [0, 642,screenY,256]. it may be more efficient to add locs here
+        sploc = []                                       #sploc is a list which determines the location of the spikes e.g. [0, 642,screenY,256]. it may be more efficient to add locs here.oly do when sure of a pos
         i = 0
         if level == 1:
-             #Place spike locations for stage 1 here
-             sploc.append((worldy, worldx // 2))
+             #Place spike locations for stage 1 here 
+             sploc.append((worldy, worldx // 2))                                  
              sploc.append((worldy // 2, worldx // 2))
              sploc.append((0, 0))
              while i < len(sploc):                                    
                 j = 0
-                while j <= sploc[i][2]:
-                    spike = Spike((sploc[i][0] + (j * tx)), sploc[i][1], tx, ty, pygame.image.load("pyGame_Image_Folder/Animation/Platformer/Obstacles/Spike/Spike1.png"))
-                    spike_list.add(spike)
+                while j <= sploc[i][2]:                        #for some reason sploc is classed as a tuple
+                    spk = Spike((sploc[i][0] + (j * tx)), sploc[i][1], tx, ty, pygame.image.load("pyGame_Image_Folder/Animation/Platformer/Obstacles/Spike/Spike1.png"))
+                    spike_list.add(spk)
                     j = j + 1
                 i = i + 1
         if level == 2:
@@ -199,12 +198,12 @@ class Level:
     #add enemy spawn loc here
 
     def platform(level, tx, ty):
-        plat_list =pygame.sprite.Group()
+        plat_list = pygame.sprite.Group()
         ploc = []                                       #ploc is a list which determines the location of the platforms e.g. [0, 642,screenY,256]
         i = 0
-        if level == 1:
-            #Place platform locations for stage 1 here
-            ploc.append((worldx // 2, worldy - ty - ty, 3))
+        if level == 1:                                                               
+            #Place platform locations for stage 1 here 
+            ploc.append((worldx-(4*ty), worldy -256, 3))
             ploc.append((worldx-768, worldy - ty - 392, 3))
             ploc.append((worldx-1024, worldy - ty - 392, 3))
             while i < len(ploc):
@@ -232,7 +231,7 @@ while i <= (worldx / tx) + tx:
     
 ground_list = Level.ground(1, gloc, tx, ty)
 plat_list = Level.platform(1, tx, ty)
-#spike_list = Level.spike(1, tx, ty)
+spike_list = Level.spike(1, tx, ty)
 
 
 player = Player() #spawn the player char
@@ -262,7 +261,7 @@ while running == True:
     batvert.draw(screen)
  #   ground_list.draw(screen)
     plat_list.draw(screen)
- #   spike_list.draw(screen)
+    spike_list.draw(screen)
 #    if pygame.Rect.colliderect(spike1.rect,player.rect):
  #       player.death()
         
